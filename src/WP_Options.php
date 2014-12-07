e071f293e89d4ce15cdea2cdb59fb0d3cc507921
Evan Mattson <me@aaemnnost.tv>
2014-12-06 23:12:26 -0500

    remove cache props

--- src/WP_Options.php
+++ src/WP_Options.php
@@ -19,21 +19,6 @@ class WP_Options implements TokenStorageInterface
     protected $stateKey;
 
     /**
-     * @var object|\Redis
-     */
-    protected $redis;
-
-    /**
-     * @var object|TokenInterface
-     */
-    protected $cachedTokens;
-
-    /**
-     * @var object
-     */
-    protected $cachedStates;
-
-    /**
      * @param string $key The key to store the token under in the options table
      * @param string $stateKey The key to store the state under in options table.
      */
@@ -41,8 +26,6 @@ class WP_Options implements TokenStorageInterface
     {
         $this->key = $key;
         $this->stateKey = $stateKey;
-        $this->cachedTokens = array();
-        $this->cachedStates = array();
     }
 
     /**
@@ -54,11 +37,7 @@ class WP_Options implements TokenStorageInterface
             throw new TokenNotFoundException('Token not found in wp_options');
         }
 
-        if (isset($this->cachedTokens[$service])) {
-            return $this->cachedTokens[$service];
-        }
-
-        return $this->cachedTokens[$service] = get_option("{$this->key}_{$service}");
+        return get_option("{$this->key}_{$service}");
     }
 
     /**
@@ -68,7 +47,6 @@ class WP_Options implements TokenStorageInterface
     {
         // (over)write the token
     	update_option("{$this->key}_{$service}", $token);
-        $this->cachedTokens[$service] = $token;
 
         // allow chaining
         return $this;
@@ -79,13 +57,7 @@ class WP_Options implements TokenStorageInterface
      */
     public function hasAccessToken($service)
     {
-        if (isset($this->cachedTokens[$service])
-            && $this->cachedTokens[$service] instanceof TokenInterface
-        ) {
-            return true;
-        }
-
-        return get_option("{$this->key}_{$service}");
+        return (get_option("{$this->key}_{$service}") instanceof TokenInterface);
     }
 
     /**
@@ -94,7 +66,6 @@ class WP_Options implements TokenStorageInterface
     public function clearToken($service)
     {
         delete_option("{$this->key}_{$service}");
-        unset($this->cachedTokens[$service]);
 
         // allow chaining
         return $this;
@@ -105,9 +76,6 @@ class WP_Options implements TokenStorageInterface
      */
     public function clearAllTokens()
     {
-        // memory
-        $this->cachedTokens = array();
-
         // clear from db..
         // 
         // 
@@ -125,11 +93,7 @@ class WP_Options implements TokenStorageInterface
             throw new AuthorizationStateNotFoundException('State not found in wp_options');
         }
 
-        if (isset($this->cachedStates[$service])) {
-            return $this->cachedStates[$service];
-        }
-
-        return $this->cachedStates[$service] = get_option("{$this->stateKey}_{$service}");
+        return get_option("{$this->stateKey}_{$service}");
     }
 
     /**
@@ -139,7 +103,6 @@ class WP_Options implements TokenStorageInterface
     {
         // (over)write the token
     	update_option("{$this->stateKey}_{$service}", $state);
-        $this->cachedStates[$service] = $state;
 
         // allow chaining
         return $this;
@@ -150,13 +113,7 @@ class WP_Options implements TokenStorageInterface
      */
     public function hasAuthorizationState($service)
     {
-        if (isset($this->cachedStates[$service])
-            && null !== $this->cachedStates[$service]
-        ) {
-            return true;
-        }
-
-        return get_option("{$this->stateKey}_{$service}");
+        return (bool) get_option("{$this->stateKey}_{$service}");
     }
 
     /**
@@ -165,7 +122,6 @@ class WP_Options implements TokenStorageInterface
     public function clearAuthorizationState($service)
     {
         delete_option("{$this->stateKey}_{$service}");
-        unset($this->cachedStates[$service]);
 
         // allow chaining
         return $this;
@@ -176,9 +132,6 @@ class WP_Options implements TokenStorageInterface
      */
     public function clearAllAuthorizationStates()
     {
-        // memory
-        $this->cachedStates = array();
-
         // clear from db..
         // 
         // 
